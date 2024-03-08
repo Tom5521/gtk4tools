a428c230f4a2f4c5e802f871bca748d4c4dd53f8
Tom5521 <thomas.bored5521@gmail.com>
2024-03-08 19:43:33 -0300

    Rename t -> tools

--- pkg/tools/main.go
+++ pkg/tools/main.go
@@ -1,6 +1,8 @@
-package t
+package tools
 
-import "github.com/diamondburned/gotk4/pkg/gtk/v4"
+import (
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+)
 
 // Converts the slice of the gtk.Widgetter types to a slice of gtk.Widgetter.
 func ToWidgetter[T gtk.Widgetter](items []T) []gtk.Widgetter {
