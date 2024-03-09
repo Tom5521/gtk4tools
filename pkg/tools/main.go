ad710379ae2c1bc74a5d7dc4b10beee7f4f07626
Tom5521 <thomas.bored5521@gmail.com>
2024-03-09 20:05:49 -0300

    Change ToWidgetter[T gtk.Widgetter](items ...T) to ToWidgetter[T gtk.Widgetter](items []T)

--- pkg/tools/main.go
+++ pkg/tools/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Converts the slice of the gtk.Widgetter types to a slice of gtk.Widgetter.
-func ToWidgetter[T gtk.Widgetter](items ...T) []gtk.Widgetter {
+func ToWidgetter[T gtk.Widgetter](items []T) []gtk.Widgetter {
 	var widgets []gtk.Widgetter
 	for _, w := range items {
 		widgets = append(widgets, w)
